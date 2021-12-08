d3cbf6a197fe6f2555d1eeeb4ee12c7bc7a62187
Elena <ivanovelens@gmail.com>
2021-12-08 22:47:36 +0300

    yet doesn't work

--- Homework Task 2 6 December/ViewController.swift
+++ Homework Task 2 6 December/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var buttonColor: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColor.layer.cornerRadius = 90
+        yellowColor.layer.cornerRadius = 90
+        greenColor.layer.cornerRadius = 90
     }
 
-
+    @IBAction func startPress() {
+    }
+    
 }
 
